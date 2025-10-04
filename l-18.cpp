3c2fb4e9ac9e1bf935906c80533d5b78f09aaa55
Annie <annmora21@gmail.com>
2025-10-04 15:29:43 -0700

    v-0.2

--- l-18.cpp
+++ l-18.cpp
@@ -2,9 +2,12 @@
 // IDE used: Visual Studio Code
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 void userChoice();
+void userRatings();
+void userComments();
 
 int main() 
 {
@@ -12,7 +15,18 @@ int main()
     cout << "[1] New nodes are added at the head of the linked list." << endl;
     cout << "[2] New nodes are added at the tail of the linked list." << endl;
     userChoice();
-    
+
+    cout << "Enter review rating (0-5): ";
+    userRatings();
+
+    cout << "Enter review comments: ";
+    userComments();
+
+    cout << "Add another review? (Y/N): ";
+    char anotherReview;
+    cin >> anotherReview;
+
+
     return 0;
 }
 
@@ -26,4 +40,22 @@ void userChoice()
         cout << "Choice: 2" << endl;
     else
         cout << "Invalid choice. Please enter either 1 or 2." << endl;
+}
+
+void userRatings()
+{
+    int rating;
+    cin >> rating;
+    if (rating >= 0 && rating <= 5)
+        cout << "Rating: " << rating << endl;
+    else
+        cout << "Invalid rating. Please enter a number between 0 and 5." << endl;
+}
+
+void userComments()
+{
+    string comments;
+    cin.ignore(); // To ignore the newline character left in the buffer
+    getline(cin, comments);
+    cout << "Comments: " << comments << endl;
 }
