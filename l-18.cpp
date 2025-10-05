33f038377cb449d0cb2bffb01b654825908511ab
Annie <annmora21@gmail.com>
2025-10-04 18:25:28 -0700

    v-0.4

--- l-18.cpp
+++ l-18.cpp
@@ -11,55 +11,23 @@ const int SIZE = 5; // Define the number of reviews to collect
 
 struct Node // Node structure for linked list
 {
-    int rating; // Rating (0-5)
+    double rating; // Rating (0-5)
     string comments; // User comments
     Node *next; // Pointer to the next node
 };
 
 // Function prototypes
 void userChoice(Node *); // Function to store and display user choice
+void outputList(Node *); // Function to output the linked list
 
 int main() 
 {
-    Node *head = nullptr; // Pointer to the head of the linked list
-    Node *tail = nullptr; // Pointer to the tail of the linked list
-    Node *current = nullptr; // Pointer for traversing the list
+    Node* head = nullptr; // Declare and initialize head pointer
 
     userChoice(head); // Call function to get user choice
 
-    for (int i = 0; i < SIZE; i++) // Loop to create nodes
-    {
-        Node* newNode = new Node; // Dynamically allocate memory for a new node
-
-        cout << "Enter review rating (0-5): ";
-        cin >> newNode->rating; // Get user rating
-        cout << "Enter review comments: ";
-        cin.ignore(); // To ignore the newline character left in the buffer
-        getline(cin, newNode->comments); // Get user comments
-
-        newNode->next = nullptr; // Initialize next pointer to nullptr
-
-        if (head == nullptr) // If the list is empty
-        {
-            head = newNode; // Set head to the new node
-            tail = newNode; // Set tail to the new node
-        }
-        else 
-        {
-            if (i % 2 == 0) // If user chose to add at head
-            {
-                newNode->next = head; // New node points to current head
-                head = newNode; // Update head to the new node
-            }
-            else // If user chose to add at tail
-            {
-                tail->next = newNode; // Current tail points to new node
-                tail = newNode; // Update tail to the new node
-            }
-        }
-    }
-
     cout << "Outputting all reviews: " << endl;
+    outputList(head); // Call function to output the linked list
 
     return 0;
 }
@@ -78,22 +46,18 @@ void userChoice(Node *choice)
         cin >> userchoice;
     if (userchoice == 1)
         cout << "Choice: 1" << endl;
-    if (userchoice == 2)
+    else if (userchoice == 2)
         cout << "Choice: 2" << endl;
-    if (userchoice != 1 && userchoice != 2)
+    else if (userchoice != 1 && userchoice != 2)
         cout << "Invalid choice. Please enter either 1 or 2." << endl;
-    }
 
     cout << "Enter review rating (0-5): ";
-    int rating;
-    while (true) // Loop to ensure valid input
-    {
+    double rating;
         cin >> rating;
     if (rating >= 0 && rating <= 5)
         cout << "Rating: " << rating << endl;
     else
         cout << "Invalid rating. Please enter a number between 0 and 5." << endl;
-    }
 
     cout << "Enter review comments: ";
     string userComment;
@@ -104,4 +68,61 @@ void userChoice(Node *choice)
     cout << "Enter another review? (Y/N): ";
     char anotherReview;
     cin >> anotherReview;
+    anotherReview = toupper(anotherReview); // Convert to uppercase for consistency
+    if (anotherReview == 'Y')
+        cout << "You chose to enter another review." << endl;
+    else if (anotherReview == 'N')
+        cout << "You chose not to enter another review." << endl;
+    else
+        cout << "Invalid choice. Please enter either Y or N." << endl;
+    }
+
+}
+
+// Outputs the linked list
+void outputList(Node *head)
+{
+    Node *current = head; // Start from the head of the list
+    Node *tail = nullptr; // Pointer to the tail of the linked list
+    for (int i = 0; i < SIZE; i++) // Loop to create nodes
+    {
+        Node* newNode = new Node; // Dynamically allocate memory for a new node
+
+        cout << "Enter review rating (0-5): ";
+        cin >> newNode->rating; // Get user rating
+        cout << "Enter review comments: ";
+        cin.ignore(); // To ignore the newline character left in the buffer
+        getline(cin, newNode->comments); // Get user comments
+
+        newNode->next = nullptr; // Initialize next pointer to nullptr
+
+        if (head == nullptr) // If the list is empty
+        {
+            head = newNode; // Set head to the new node
+            tail = newNode; // Set tail to the new node
+            current = head; // Update current pointer
+        }
+        else 
+        {
+            if (i % 2 == 0) // If user chose to add at head
+            {
+                newNode->next = head; // New node points to current head
+                head = newNode; // Update head to the new node
+                current = head; // Update current pointer
+            }
+            else // If user chose to add at tail
+            {
+                tail->next = newNode; // Current tail points to new node
+                tail = newNode; // Update tail to the new node
+            }
+        }
+    }
+
+    current = head; // Reset current to head for traversal
+    while (current != nullptr) // Traverse the list
+    {
+        cout << "Rating: " << current->rating << endl; // Output rating
+        cout << "Comments: " << current->comments << endl; // Output comments
+        current = current->next; // Move to the next node
+    }
 }
